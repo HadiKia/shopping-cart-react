2daee3894f05410ad1b6aead1ef62e41632a5cab
HadiKia <hadikiamarzi@protonmain.com>
2023-05-08 17:44:50 +0330

    cart page created

--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 
 // Components
 import Navbar from "./shared/Navbar";
+import Cart from "./shared/Cart";
 
+// Context
+import { CartContext } from "../context/CartContextProvider";
 
 const ShopCart = () => {
+  const { state, dispatch } = useContext(CartContext);
+
   return (
     <div>
       <Navbar />
-       cart
+
+      <div>
+        {state.selectedItems.map((item) => (<Cart key={item.id} data={item} />))}
+      </div>
+
+      {state.itemsCounter > 0 && (
+        <div>
+          <p><span>Total Items: </span> {state.itemsCounter}</p>
+          <p><span>Total Payments: </span> {state.total} $</p>
+          <div>
+            <button onClick={() => dispatch({ type: "CHECKOUT" })}>Check Out</button>
+            <button onClick={() => dispatch({ type: "CLEAR" })}>Clear</button>
+          </div>
+        </div>
+      )}
+      {/* checkout button */}
+      {state.checkout && (
+        <div>
+          <h3>Checked Out Successfully</h3>
+          <Link to="/products">Buy More</Link>
+        </div>
+      )}
+      {/* clear button */}
+      {!state.checkout && state.itemsCounter === 0 && (
+        <div>
+          <h3>Want to buy? :)</h3>
+          <Link to="/products">Shop</Link>
+        </div>
+      )}
     </div>
   );
 };
--- src/context/CartContextProvider.js
+++ src/context/CartContextProvider.js
@@ -25,7 +25,8 @@ const cartReducer = (state, action) => {
       return {
         ...state,
         selectedItems: [...state.selectedItems],
-        ...sumItems(state.selectedItems)
+        ...sumItems(state.selectedItems),
+        checkout: false
       };
 
     case "REMOVE_ITEM":
@@ -35,7 +36,7 @@ const cartReducer = (state, action) => {
       return {
         ...state,
         selectedItems: [...newSelectedItems],
-        ...sumItems(state.selectedItems)
+        ...sumItems(newSelectedItems)
       };
 
     case "INCREASE":
